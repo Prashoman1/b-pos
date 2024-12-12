b764de073025aa91f17dfc3650cef64b887ac02e
Prashoman1 <prashoman.mysoftheaven@gmail.com>
2024-12-12 14:43:11 +0600

    reusable input used

--- app/(home)/_components/MenuBar/MenuBar.tsx
+++ app/(home)/_components/MenuBar/MenuBar.tsx
@@ -10,6 +10,7 @@ import { FiLogOut } from "react-icons/fi";
 
 import SunLogo from "@/components/shared/SunLogo";
 import ExpandIcon from "@/components/shared/ExpandIcon";
+import { Input } from "@/components/ui/input";
 
 const MenuBar = () => {
   return (
@@ -17,16 +18,13 @@ const MenuBar = () => {
       <div className="bg-white py-5 w-full  px-16">
         <div className="flex items-center gap-3">
           <div className="w-[38%] relative">
-            <input
-              className="border w-full py-2 ps-10 pe-16 rounded focus:outline-none border-success bg-white text-13 placeholder:text-black placeholder:font-medium select-none"
-              type="text"
-              placeholder="Select Customer..."
-            />
+            <Input type="text" name="userName" className="ps-14 pe-14 placeholder:text-13 placeholder:font-semibold placeholder:text-black" placeholder="Select Customer..." />
+           
             <span className="absolute top-1/2 left-3 transform -translate-y-1/2">
               <FiUser className="text-18 text-black" />
             </span>
             <span className="bg-primary rounded-r absolute top-1/2 right-[2px] cursor-pointer transform -translate-y-1/2 px-3 py-2 border-success">
-              <FiUserPlus className="text-white text-18" />
+              <FiUserPlus type="submit" className="text-white text-18" />
             </span>
           </div>
           <div className="w-[38%]">
